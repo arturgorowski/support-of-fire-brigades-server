0ef1a6f28e0de12edaa05ed9ed78740ecf1fa00f
Artur <artek159@gmail.com>
2019-08-30 14:45:14 +0200

    create new user roles;

--- common/models/firefighter.js
+++ common/models/firefighter.js
@@ -1,4 +1,4 @@
-module.exports = function(Firefighter) {
+module.exports = function (Firefighter) {
 
     'use strict';
 
@@ -6,27 +6,82 @@ module.exports = function(Firefighter) {
     const path = require("path");
     //const ObjectID = app.dataSources.mongo.ObjectID;
 
-    /**
-     * 
-     * create user role
-     */
-    Firefighter.observe("after save", function (ctx, next){
 
-        //if new user we have to create roles
-        if(ctx.isNewInstance){
+    // --------------------------- OBSERVERS ---------------------------
+    /**
+     *
+     *  create user role
+     *
+     */
+    Firefighter.observe("after save", function (ctx, next) {
+
+        // if new user we have to create roles
+        if (ctx.isNewInstance) {
+
+            const Firefighter = app.models.Firefighter;
 
             let roleName = "", promises = [];
             let Role = app.models.Role;
             let RoleMapping = app.models.RoleMapping;
 
-            // PrezesOSP registration process
-            if(ctx.instance.code){
+            // TODO sprawdzenie kto jest zalogowany, jak prezes, to przy tworzeniu nowych uzytkowników dodaje userów z ROLĄ strazak
+            let personalSettings//, isPrezesOSPLogged = ctx.options.accessToken && (ctx.options.accessToken.authorizedRoles.children[0] === true), promiseBuilder = [];
 
+            console.log("ctx.options.accessToken>>>>", ctx.options.accessToken.authorizedRoles)
+            console.log("ctx.options.accessToken>>>>", ctx.options.accessToken)
+            console.log("ctx.options>>>>", ctx.options)
+            if (ctx.instance.role === "prezesOSP" && isPrezesOSPLogged) {
+  
+              roleName = "strazak";
+              personalSettings = {
+                role: roleName,
+                emailVerified: false
+              };
+  
+            } else {
+  
                 roleName = "prezesOSP";
-
-
+                personalSettings = {
+                    role: roleName,
+                    emailVerified: false
+                };
             }
+
+            ctx.instance.updateAttributes(personalSettings).then(function (model) {
+
+                return model;
+
+            }).catch(function (err) {                // ...
+                console.error(">>> ERR :: error when registerig user", err);
+                return err;
+            });
+
+
+            // --------------------------- CREATE ROLES ---------------------------
+
+            Role.findOrCreate({
+                where: { name: roleName }
+            }, {
+                    name: roleName
+                }, function (err, role) {
+                    if (!err) {
+                        role.principals.create({
+                            principalType: RoleMapping.USER,
+                            principalId: ctx.instance.id.toString()
+                        }, function (err, principal) {
+                            if (err) console.error(">>> ERR :: CREATE ROLE MAPPING", err, roleName);
+                        });
+
+                        return next();
+                    } else {
+                        console.error(">>> ERR :: CREATE ROLE ", err, roleName);
+                        return next(err);
+                    }
+                });
+
+        } else {
+            return next();
         }
-    })
+    });
 
 };
